b49bd5a90c9ed42cf011bc951023d176a5e95825
sambhav <sambhavgupta4@gmail.com>
2024-01-16 01:05:14 +0530

    Add Themes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import BasicGrid from './BasicGrid.js'
 import DataGrid1 from './DataGrid1.js'
+import ThemeUsage from './ThemeUsage.js'
+
+
 function App() {
   return (
     <div className="App">
       Hello World
       <BasicGrid />
       <DataGrid1 />
+      <ThemeUsage />
     </div>
   );
 }
